ade0f4f2f86d047e2800c4fd21cb5f6d7d8812fd
Liu Jingkai <rock_jk@msn.com>
2021-05-12 14:04:36 +0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Guadoo on 2021/5/12.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
